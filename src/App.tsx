aa60cc54afd450829217f071f2cd9a3742aab16b
Gabriel Hijazi <hijazi.root@gmail.com>
2025-06-04 15:10:11 -0400

    create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import Text from './components/text';
 
-import TrashIcon from './assets/icons/Trash-Regular.svg?react';
-import SpinIcon from './assets/icons/spinner.svg?react';
-import PlusIcon from './assets/icons/Plus-Regular.svg?react';
+import TrashIcon from './assets/icons/trash-regular-icon.svg?react';
+import SpinIcon from './assets/icons/spinner-icon.svg?react';
+import PlusIcon from './assets/icons/plus-regular-icon.svg?react';
+import PencilIcon from './assets/icons/pencil-simple-regular-icon.svg?react';
+import SquareDuotoneIcon from './assets/icons/square-duotone-icon.svg?react';
+import ClipBoardIcon from './assets/icons/clipboard-text-icon.svg?react';
+import XIcon from './assets/icons/x-icon.svg?react';
 import Icon from './components/icon';
 import Badge from './components/badge';
 import Button from './components/button';
+import ButtonIcon from './components/button-icon';
+import InputText from './components/input-text';
+import InputCheckbox from './components/input-checkbox';
 
 export default function App() {
   return (
     <div>
-      <div>
+      <div className="flex gap-10 p-4">
         <Text as="h1" variant="body-md" className="text-green-base">
           Teste
         </Text>
@@ -21,18 +28,38 @@ export default function App() {
           Teste
         </Text>
       </div>
-      <div>
+
+      <div className="flex gap-10 p-4">
         <Icon svg={TrashIcon} />
-        <Icon svg={SpinIcon} width="100px" animate />
+        <Icon svg={PencilIcon} />
+        <Icon svg={PlusIcon} />
+        <Icon svg={SquareDuotoneIcon} />
+        <Icon svg={XIcon} />
+        <Icon svg={ClipBoardIcon} />
+        <Icon svg={SpinIcon} width="30px" animate />
       </div>
 
-      <div>
+      <div className="flex gap-10 p-4">
         <Badge variant="secondary">5</Badge>
         <Badge variant="primary">2 de 5</Badge>
       </div>
 
       <div>
-        <Button icon={PlusIcon}>Fala comigo bb</Button>
+        <Button icon={PlusIcon}>Teste de Botão</Button>
+      </div>
+
+      <div className="flex gap-10 p-4">
+        <ButtonIcon icon={TrashIcon} />
+        <ButtonIcon icon={PlusIcon} variant="secondary" />
+        <ButtonIcon icon={TrashIcon} variant="tertiary" />
+      </div>
+
+      <div className="flex gap-10 p-4">
+        <InputText />
+      </div>
+
+      <div className="flex gap-10 p-4">
+        <InputCheckbox />
       </div>
     </div>
   );
